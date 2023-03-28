15306fa39dc1459fc833fb88664c208f757f7d98
Irene <irenefl897@gmail.com>
2023-03-28 09:50:10 +0200

    added tabs

---
+++ app/Http/Controllers/PromotionsController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Promotions;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PromotionsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Promotions $promotions)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Promotions $promotions)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Promotions $promotions)
+    {
+        //
+    }
+}
