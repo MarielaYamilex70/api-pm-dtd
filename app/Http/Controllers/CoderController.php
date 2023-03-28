cb5ff8f52a719454ea438f835c449bb45d223f5a
unknown <marielayamilex@yahoo.es>
2023-03-28 09:50:46 +0200

    Add Coders Recuiter Companie Language Stack

---
+++ app/Http/Controllers/CoderController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Coder;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CoderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Coder $coder)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Coder $coder)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Coder $coder)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/LanguageController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Language;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LanguageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Language $language)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Language $language)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Language $language)
+    {
+        //
+    }
+}
