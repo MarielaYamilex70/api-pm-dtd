3450951b5b20ec406b4fc03a997a4099ce890fc0
unknown <marielayamilex@yahoo.es>
2023-03-30 11:19:12 +0200

    Added Matches Table

--- app/Models/Coder.php
+++ app/Models/Coder.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Coder extends Model
 {
     use HasFactory;
+    public function recruiters()
+        {
+        return $this->belongsToMany(Recruiter::class, 'matches');
+    }
 }
---
+++ database/migrations/2023_03_30_091116_create_matches_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('coder_id');
+            $table->foreign('coder_id')->references('id')->on('coders');
+
+            $table->unsignedBigInteger('recuiter_id');
+            $table->foreign('recuiter_id')->references('id')->on('recuiters');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('matches');
+    }
+};
