fc5d4d949cb7df577af8598501f3602582751af1
unknown <marielayamilex@yahoo.es>
2023-03-31 12:53:49 +0200

    Added PromotionsController CRUD

--- app/Http/Controllers/PromotionsController.php
+++ app/Http/Controllers/PromotionsController.php
@@ -13,7 +13,8 @@ class PromotionsController extends Controller
      */
     public function index()
     {
-        //
+        $promotions = Promotions::all();
+        return response()->json($promotions);
     }
 
     /**
@@ -21,7 +22,26 @@ class PromotionsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'school_id' => 'required',
+            'name' => 'required',
+            'nick' => 'required',
+            'quantity' => 'required'
+
+        ]);
+
+        $promo = new Promotions;
+        $promo->school_id = $request->school_id;
+        $promo->name = $request->name;
+        $promo->nick = $request->nick;
+        $promo->quantity = $request->quantity;
+
+        $promo->save();
+        $data = [
+            'message' => 'Promotion created successfully',
+            'promotions' => $promo
+        ];
+        return response()->json($data);
     }
 
     /**
@@ -29,7 +49,11 @@ class PromotionsController extends Controller
      */
     public function show(Promotions $promotions)
     {
-        //
+        $data = [
+            'message' => 'Promotion details',
+            'promotions' => $promotions
+        ];
+        return response()->json($data);
     }
 
     /**
@@ -37,7 +61,25 @@ class PromotionsController extends Controller
      */
     public function update(Request $request, Promotions $promotions)
     {
-        //
+        $request->validate([
+            'school_id' => 'required',
+            'name' => 'required',
+            'nick' => 'required',
+            'quantity' => 'required'
+
+        ]);
+
+        $promotions->school_id = $request->school_id;
+        $promotions->name = $request->name;
+        $promotions->nick = $request->nick;
+        $promotions->quantity = $request->quantity;
+
+        $promotions->save();
+        $data = [
+            'message' => 'Promotion updated successfully',
+            'promotions' => $promotions
+        ];
+        return response()->json($data);
     }
 
     /**
@@ -45,6 +87,11 @@ class PromotionsController extends Controller
      */
     public function destroy(Promotions $promotions)
     {
-        //
+        $promotions->delete();
+        $data = [
+            'message' => 'Promotion deleted successfully',
+            'promotion' => $promotions
+        ];
+        return response()->json($data);
     }
 }
