b37de3dce65e5756a895f387fca6f1e82a1f5402
Irene <irenefl897@gmail.com>
2023-04-21 09:38:58 +0200

    all test pass

---
+++ tests/Feature/StackControllerTest.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Stack;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Response;
+use Tests\TestCase;
+
+class StackControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testCanCreateStack()
+    {
+        $stackData = [
+            'name' => 'Test Stack'
+        ];
+
+        $response = $this->postJson('/api/stacks', $stackData);
+
+        $response->assertStatus(Response::HTTP_OK)
+            ->assertJsonStructure([
+                'message',
+                'stack' => [
+                    'id',
+                    'name',
+                    'created_at',
+                    'updated_at'
+                ]
+            ])
+            ->assertJson([
+                'message' => 'Stack created successfully',
+                'stack' => $stackData
+            ]);
+    }
+
+    public function testCanGetStack()
+    {
+        $stack = Stack::factory()->create();
+
+        $response = $this->getJson("/api/stacks/{$stack->id}");
+
+        $response->assertStatus(Response::HTTP_OK)
+            ->assertJsonStructure([
+                'message',
+                'stack' => [
+                    'id',
+                    'name',
+                    'created_at',
+                    'updated_at'
+                ]
+            ])
+            ->assertJson([
+                'message' => 'Stack details',
+                'stack' => $stack->toArray()
+            ]);
+    }
+
+    public function testCanUpdateStack()
+    {
+        $stack = Stack::factory()->create();
+
+        $stackData = [
+            'name' => 'Updated Stack'
+        ];
+
+        $response = $this->putJson("/api/stacks/{$stack->id}", $stackData);
+
+        $response->assertStatus(Response::HTTP_OK)
+            ->assertJsonStructure([
+                'message',
+                'stack' => [
+                    'id',
+                    'name',
+                    'created_at',
+                    'updated_at'
+                ]
+            ])
+            ->assertJson([
+                'message' => 'Stack updated successfully',
+                'stack' => $stackData
+            ]);
+
+        $this->assertDatabaseHas('stacks', $stackData);
+    }
+
+    public function testCanDeleteStack()
+    {
+        $stack = Stack::factory()->create();
+
+        $response = $this->deleteJson("/api/stacks/{$stack->id}");
+
+        $response->assertStatus(Response::HTTP_OK)
+            ->assertJsonStructure([
+                'message',
+                'stack' => [
+                    'id',
+                    'name',
+                    'created_at',
+                    'updated_at'
+                ]
+            ])
+            ->assertJson([
+                'message' => 'Stack delete successfully',
+                'stack' => $stack->toArray()
+            ]);
+
+        $this->assertDatabaseMissing('stacks', $stack->toArray());
+    }
+}
