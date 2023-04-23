090276cf28834a71ea5af90aeba8f3ec4da0b677
Irene <irenefl897@gmail.com>
2023-04-23 17:53:00 +0200

    img

---
+++ All/2023_03_28_072842_create_promotions_table.php
@@ -0,0 +1,69 @@
+<?php
+
+// use Illuminate\Database\Migrations\Migration;
+// use Illuminate\Database\Schema\Blueprint;
+// use Illuminate\Support\Facades\Schema;
+
+// return new class extends Migration
+// {
+    /**
+     * Run the migrations.
+     */
+    // public function up(): void
+    // {
+    //     Schema::create('promotions', function (Blueprint $table) {
+    //         $table->id();
+    //         $table->unsignedBigInteger('school_id');
+    //         $table->foreign('school_id')->references('id')->on('schools');            
+
+    //         $table->string('name');
+    //         $table->string('nick');
+    //         $table->string('quantity');
+    //         $table->timestamps();
+    //     });
+    // }
+
+    /**
+     * Reverse the migrations.
+     */
+//     public function down(): void
+//     {
+//         Schema::dropIfExists('promotions');
+//     }
+// };
+
+
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePromotionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('promotions', function (Blueprint $table) {
+            $table->id();
+            
+            // $table->unsignedBigInteger('school_id');
+            // $table->foreign('school_id')->references('id')->on('schools');
+
+            $table->string('name');
+            $table->string('nick');
+            $table->string('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('promotions');
+    }
+}
+
